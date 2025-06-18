eb47fabf0cb67944cdd0a6f2ce90d0997cf6e6f7
Russell Purinton <russell.purinton@gmail.com>
2025-06-18 22:12:14 +0000

    Pushed 2025-06-18 22:12:14

--- clockbot.mjs
+++ clockbot.mjs
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import 'dotenv/config';
-//import { createDb } from '@purinton/mysql';
 import { createDiscord } from '@purinton/discord';
 import { log, fs, path, registerHandlers, registerSignals } from '@purinton/common';
 
@@ -12,20 +10,17 @@ const version = packageJson.version;
 
 const presence = { activities: [{ name: `clockbot v${version}`, type: 4 }], status: 'online' };
 
-//const db = await createDb({ log });
-//registerSignals({ shutdownHook: () => db.end() });
 const client = await createDiscord({
     log,
     rootDir: path(import.meta),
     context: {
-        //db,
         presence,
         version
     },
     intents: {
         Guilds: true,
         GuildMessages: true,
-        MessageContent: false,
+        MessageContent: true,
         GuildMembers: false,
         GuildPresences: false,
         GuildVoiceStates: false,
